0e2555dd9ca5f0af05867c55b64c0abbe27a8ca9
muhammad zeeshan <shaniservices4@gmail.com>
2022-11-03 17:52:04 +0000

    Main files

---
+++ script.js
@@ -0,0 +1,17 @@
+// For FadeIn Animation
+const elements = document.querySelectorAll('.about-animation')
+
+const animate = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        entry.target.classList.toggle('animate__fadeIn', entry.isIntersecting)
+        if (entry.isIntersecting) animate.unobserve(entry.target)
+    });
+},{
+    threshold: .5
+});
+
+
+elements.forEach(element => {
+    animate.observe(element)
+})
+
