b53115b5f47755be06211ed3bab42c6722664862
muhammad zeeshan <shaniservices4@gmail.com>
2022-11-05 17:38:26 +0000

    Added Some Animation

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-// For FadeIn Animation
-const elements = document.querySelectorAll('.about-animation')
+// // For FadeIn Animation
+// const elements = document.querySelectorAll('.about-animation')
 
-const animate = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        entry.target.classList.toggle('animate__fadeIn', entry.isIntersecting)
-        if (entry.isIntersecting) animate.unobserve(entry.target)
-    });
-},{
-    threshold: .5
-});
+// const animate = new IntersectionObserver(entries => {
+//     entries.forEach(entry => {
+//         entry.target.classList.toggle('animate__fadeIn', entry.isIntersecting)
+//         if (entry.isIntersecting) animate.unobserve(entry.target)
+//     });
+// },{
+//     threshold: .5
+// });
 
 
-elements.forEach(element => {
-    animate.observe(element)
-})
+// elements.forEach(element => {
+//     animate.observe(element)
+// })
 
